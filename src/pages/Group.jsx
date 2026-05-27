9a0db063890bdbc4c6108b4940a41de5afdead95
LMAZarate <122065836+LMAZarate@users.noreply.github.com>
2026-05-27 11:58:25 -0300

    Update Group.jsx

--- src/pages/Group.jsx
+++ src/pages/Group.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { useState, useEffect, useCallback } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { supabase } from '../lib/supabase'
